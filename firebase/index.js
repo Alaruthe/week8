eeb32fd5aaa25df5f799b570d1a1dca2ecd74ede
Alaruthe <akionishimura60@gmail.com>
2022-10-25 16:16:44 -0700

    um

--- firebase/index.js
+++ firebase/index.js
@@ -5,12 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCE1124BHs7kKEFQK9zTkSbmySh5ctZnHs",
-  authDomain: "f22-week7.firebaseapp.com",
-  projectId: "f22-week7",
-  storageBucket: "f22-week7.appspot.com",
-  messagingSenderId: "823450911667",
-  appId: "1:823450911667:web:1de21a452db9ab461ac1c6"
+  apiKey: "AIzaSyCKUqsr5ZmN6-BYa-HwjEodghOVpoqcq_c",
+  authDomain: "week9-10-eef8d.firebaseapp.com",
+  projectId: "week9-10-eef8d",
+  storageBucket: "week9-10-eef8d.appspot.com",
+  messagingSenderId: "311110253754",
+  appId: "1:311110253754:web:26608b329338e437bb9683",
+  measurementId: "G-95EYEJVQVP"
 };
 
 // Initialize Firebase
