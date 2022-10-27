b6a59d53c649e63c7a5ea4e4fbc6ede152cac33b
Alaruthe <akionishimura60@gmail.com>
2022-10-26 17:40:07 -0700

    eep

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import firebase from "../../lib/firebase";
+
+export default async function handler(req, res) {
+  try{
+    const snapshot = await firebase.collection("tester").get();
+
+    let output = [];
+
+    snapshot.forEach(
+      (doc) => {
+        output.push(
+          {
+            id: doc.id,
+            data: doc.data()
+          }
+        );
+      }
+    );
+
+    console.log(output);
+  } catch(error){
+    console.error(err);
+    res.status(500).end(error.message);
+  }
 }
